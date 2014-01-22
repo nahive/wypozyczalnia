f0ba2eb37027faea55c098dade0e1323d4750712
Rudy2992 <mikolaj2992@gmail.com>
2014-01-22 20:13:07 +0100

    Baza Danych
    Zrobione większość pól
    Protokół nie uwzględnia jeszcze Firmy

--- Wypozyczalnia/Wypoz/admin.py
+++ Wypozyczalnia/Wypoz/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from Wypoz.models import Auto
-from Wypoz.models import Pracownik
-from Wypoz.models import Protokol
+from Wypoz.models import *
 
-admin.site.register(Auto)
+#admin.site.register(Auto)
 admin.site.register(Pracownik)
 admin.site.register(Protokol)
+admin.site.register(Klient)
+admin.site.register(Firma)
+
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'num_rej')
+
+admin.site.register(Auto, AutoAdmin)
--- Wypozyczalnia/Wypoz/models.py
+++ Wypozyczalnia/Wypoz/models.py
@@ -1,36 +1,84 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
 
 # Create your models here.
 class Auto(models.Model):
-    IdSamochodu = models.CharField(max_length=200)
-    Marka = models.CharField(max_length=200)
-    Model = models.CharField(max_length=200)
-    Klasa = models.CharField(max_length=200)
-    NumerRej = models.CharField(max_length=200)
-    Wyposaz = models.CharField(max_length=200)
-    Silnik = models.CharField(max_length=200)
-    Kolor = models.CharField(max_length=200)
-    LiczbaSiedz = models.CharField(max_length=200)
-    Cena = models.CharField(max_length=200)
-    TerminWyp = models.CharField(max_length=200)
-    TerminOdd = models.CharField(max_length=200)
-    NrDowodu = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+    marka = models.CharField('Marka', max_length=50)
+    model = models.CharField('Model', max_length=30)
+    klasa = models.CharField('Klasa', max_length=30)
+    num_rej = models.CharField('Numer rejestracyjny', max_length=10)
+    wypos = models.TextField('Wyposazenie')
+    silnik = models.CharField('Silnik', max_length=30)
+    kolor = models.CharField('Kolor', max_length=20)
+    licz_siedz = models.IntegerField('Liczba siedzen')
+    cena = models.DecimalField('Cena', max_digits=10, decimal_places=2)
+    data_wyp = models.DateField('Data wypozyczenia', blank=True, null=True)
+    data_odd = models.DateField('Data oddania', blank=True, null=True)
+    num_dow = models.CharField('Numer dowodu', max_length=9, blank=True, null=True, 
+                                 validators=[RegexValidator(regex='^[A-Z]{3}[0-9]{6}', message='Numer postaci AAA999999')])
+    def __unicode__(self):
+        return str(self.num_rej) + ' ' + str(self.model)
 
 class Pracownik(models.Model):
-    Imie = models.CharField(max_length=200)
-    Nazwisko = models.CharField(max_length=200)
-    Ulica = models.CharField(max_length=200)
-    Miasto = models.CharField(max_length=200)
-    DataUr = models.CharField(max_length=200)
-    NrDowoduPrac = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+    imie = models.CharField('Imie', max_length=20)
+    naz = models.CharField('Nazwisko', max_length=30)
+    miasto = models.CharField('Miasto', max_length=30)
+    ulica = models.CharField('Ulica', max_length=30)
+    data_ur = models.DateField('Data urodzenia')
+    num_dow = models.CharField('Numer dowodu', max_length=9, 
+                                 validators=[RegexValidator(regex='^[A-Z]{3}[0-9]{6}', message='Numer postaci AAA999999')])
 
     def odbierz():
         return 1
 
 class Protokol(models.Model):
-    IdSam = models.CharField(max_length=200)
-    IdProt = models.CharField(max_length=200)
-    NrDow = models.CharField(max_length=200)
-    Data = models.CharField(max_length=200)
-    Opis = models.CharField(max_length=200)
-    Wycena = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+    id_sam = models.ForeignKey('Wypoz.Auto')
+    num_dow = models.ForeignKey('Wypoz.Klient')
+    #num_dow = models.ForeignKey(ContentType)
+    #object_id = models.PositiveIntegerField()
+    #content_object = generic.GenericForeignKey('num_dow', 'object_id')
+    #num_dow = models.CharField('Numer Dowodu', max_length=9, 
+                                 #validators=[RegexValidator(regex='^[A-Z]{3}[0-9]{6}', message='Numer postaci AAA999999')])
+    data = models.DateField('Data')
+    opis = models.TextField('Opis')
+    wycena = models.DecimalField('Wycena', max_digits=10, decimal_places=2)
+    
+
+class Klient(models.Model):
+    id = models.AutoField(primary_key=True)
+    imie = models.CharField('Imie', max_length=20)
+    naz = models.CharField('Nazwisko', max_length=30)
+    miasto = models.CharField('Miasto', max_length=30)
+    ulica = models.CharField('Ulica', max_length=30)
+    email = models.EmailField('E-Mail', blank=True, null=True)
+    tel = models.CharField('Telefon', max_length=9, 
+                           validators=[RegexValidator(regex='^[0-9]', message='Max 9 cyfr')])
+    num_dow = models.CharField('Numer dowodu', max_length=9, 
+                                 validators=[RegexValidator(regex='^[A-Z]{3}[0-9]{6}', message='Numer postaci AAA999999')])
+    stat = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return str(self.id) + ' ' + str(self.num_dow)
+
+class Firma(models.Model):
+    id = models.AutoField(primary_key=True)
+    naz_pel = models.CharField('Nazwa', max_length=50, help_text='Pelna nazwa firmy')
+    naz_skr = models.CharField('Skrot', max_length=20, help_text='Skrocona nazwa firmy',  blank=True, null=True)
+    nip = models.CharField('NIP', max_length=10, 
+                                 validators=[RegexValidator(regex='^[0-9]{10}', message='Numer w postaci 10 cyfr')])
+    reg = models.CharField('REGON', max_length=14, 
+                                 validators=[RegexValidator(regex='^[0-9]', message='Numer w postaci 9 lub 14 cyfr')])
+    rodz_dzial = models.CharField('Rodzaj dzialalnosci', max_length=50, blank=True, null=True)
+    num_dow_p = models.CharField('Numer dowodu', max_length=9, help_text='Numer dowodu tozsamosci przedstawiciela firmy',  
+                                 validators=[RegexValidator(regex='^[A-Z]{3}[0-9]{6}', message='Numer postaci AAA999999')])
+    imie_p = models.CharField('Imie', max_length=20, help_text='Imie przedstawiciela firmy')
+    naz_p = models.CharField('Nazwisko', max_length=30, help_text='Nazwisko przedstawiciela firmy')
+
+    def __unicode__(self):
+        return str(self.naz_pel) + ' ' + str(self.reg)
